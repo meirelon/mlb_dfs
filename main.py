6f9fc50532e7dc1aa5d0d44d8940914d7583e9f0
meirelon <nestelm@gmail.com>
2019-04-29 20:42:45 -0400

    no index

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def file_to_gcs(request):
     request_json = request.get_json(silent=True)
     if request_json and "dk" in request_json:
         df = pd.read_csv(request_json.get("dk"))
-        df.to_csv("/tmp/dk.csv")
+        df.to_csv("/tmp/dk.csv", index=False)
         upload_blob(bucket_name=bucket,
                     source_file_name="/tmp/dk.csv",
                     destination_blob_name="mlb_{}.csv".format(today.replace("-","")))
