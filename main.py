e71b901bda386c513f2e2096ad829428b40bad99
meirelon <nestelm@gmail.com>
2019-05-01 13:59:47 -0400

    changed dataset

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ def dk_to_gcp(request):
 
 def dk_predictions(request):
     project = os.environ["PROJECT_ID"]
-    dataset = os.environ["DATASET"]
-    dfs_dataset = os.environ["DATASET_DFS"]
+    dataset_base = os.environ["DATASET_BASE"]
+    dataset_dfs = os.environ["DATASET_DFS"]
     bucket = os.environ["BUCKET"]
     model_name = os.environ["MODEL"]
     today = (datetime.now() - timedelta(hours=4)).strftime('%Y-%m-%d')
@@ -60,14 +60,14 @@ def dk_predictions(request):
                           destination_path=model_name,
                           filename=model_name)
     input_run = inputData(project=project,
-                          dataset=dataset,
+                          dataset=dataset_base,
                           yesterday=yesterday,
                           today=today)
     df = input_data.run()
     df["prediction"] = model.predict(df.drop(["name", "tm"], axis=1))
     prediction_df = df[["name", "tm", "prediction"]]
     pandas_gbq.to_gbq(df, project_id=project,
-              destination_table="{dataset}.mlb_draftkings_predictions_{dt}".format(dataset=dfs_dataset, dt=today.replace("-","")),
+              destination_table="{dataset}.mlb_draftkings_predictions_{dt}".format(dataset=dataset_dfs, dt=today.replace("-","")),
               if_exists="replace")
 
 def dk_lineups(request):
